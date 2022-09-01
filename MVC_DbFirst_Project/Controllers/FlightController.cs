1c3ae6c1018607d2b9ddaede8691ba5abbb379c6
VelitToparli <vtoparli@gmail.com>
2022-09-01 22:25:03 +0300

    Proje dosyası ekle.

---
+++ MVC_DbFirst_Project/Controllers/FlightController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DbFirst_Project.Models;
+
+namespace MVC_DbFirst_Project.Controllers
+{
+    public class FlightController : Controller
+    {
+        // GET: Flight
+
+        DbFlightEntities db = new DbFlightEntities();
+
+        public ActionResult CityList()
+        {
+            var values = db.TblCity.ToList();
+            return View(values);
+        }
+
+        public ActionResult PassengerList()
+        {
+            var values = db.TblPassenger.ToList();
+            return View(values);
+        }
+    }
+}
